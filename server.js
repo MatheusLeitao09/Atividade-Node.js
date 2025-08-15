ae4cfb6656f15bf8cbaca83cfd04872c95686894
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-08-15 20:57:12 -0300

    feat: Adiciona link de acesso ao terminal na inicialização do servidor

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ import express from "express";
 import bruxos from "./src/data/bruxos.js";
 
 const app = express();
+const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Vamos de Harry Potter");
+    res.send("Bem vindo ao Mundo de Harry Potter!");
 });
 
 app.get("/bruxos", (req, res) => {
@@ -12,5 +13,5 @@ app.get("/bruxos", (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log("🧙‍♂️ API dos Bruxos está no ar na porta 3000!");
+    console.log(`🧙‍♂️ API dos Bruxos está no ar na porta 3000! Acesse http://localhost:${PORT}`);
 });
