ab15b23d855aadb7f8eb3cde34249378911c9636
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-08-25 10:42:02 -0300

    Atualiza o bruxos

--- src/data/dados.js
+++ src/data/dados.js
@@ -19,8 +19,43 @@ const bruxos = [
         patrono: "Lontra",
         especialidade: "Saber tudo de tudo",
         vivo: true
-    }
-];
+    },
+   
+    {
+    id: 3,
+    nome: "Ron Weasley",
+    casa: "Grifinória",
+    ano: 7,
+    varinha: "Salgueiro e crina de unicórnio, 35cm",
+    mascote: "Rato (Perebas)",
+    patrono: "Terrier Jack Russell",
+    especialidade: "Xadrez Bruxo",
+    vivo: true
+
+    },
+    {
+    id: 4,
+    nome: "Draco Malfoy",
+    casa: "Sonserina",
+    ano: 7,
+    varinha: "Espinheiro e crina de unicórnio, 25cm",
+    mascote: "Coruja (não nomeada)",
+    patrono: "Desconhecido",
+    especialidade: "Ser irritante",
+    vivo: false
+    },
+    {
+    id: 5,
+    nome: "Luna Lovegood",
+    casa: "Corvinal",
+    ano: 6,
+    varinha: "Madeira de cerejeira e núcleo desconhecido",
+    mascote: "Nenhum",
+    patrono: "Lebre",
+    especialidade: "Ver criaturas imaginárias (ou não) 👻",
+    vivo: true
+    },
+    ];
 
 const casas = [
     {
